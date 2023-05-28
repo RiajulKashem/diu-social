0bb7ef1220aaabdbb2b32ff22ec2f04da6f68503
Riajul Kashem <riajul.programmer@gmail.com>
2023-05-29 00:07:33 +0600

    Add files via upload

---
+++ users/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+""" Model for User Profile """
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_online = models.BooleanField(default=False)
+    following = models.ManyToManyField(
+        User, related_name="following", blank=True
+    )
+    friends = models.ManyToManyField(
+        User, related_name='my_friends', blank=True
+    )
+    designation = models.CharField(
+        default="", blank=True, null=True, max_length=350
+    )
+    bio = models.CharField(default="", blank=True, null=True, max_length=350)
+    date_of_birth = models.CharField(blank=True, max_length=150)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(
+        default='default.jpg', upload_to='profile_pics', blank=True, null=True
+    )
+
+    def profile_posts(self):
+        return self.user.post_set.all()
+
+    def get_friends(self):
+        return self.friends.all()
+
+    def get_friends_no(self):
+        return self.friends.all().count()
+
+    def __str__(self):
+        if self.user.first_name == '' and self.user.last_name == '':
+            return self.user.username
+        return self.user.first_name + ' ' + self.user.last_name
+
+
+STATUS_CHOICES = (
+    ('send', 'send'),
+    ('accepted', 'accepted')
+)
+
+
+class Relationship(models.Model):
+    sender = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name='friend_sender'
+    )
+    receiver = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name='friend_receiver'
+    )
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sender}-{self.receiver}-{self.status}"
